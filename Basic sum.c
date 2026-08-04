ab78345fe742d3117dbe62d5b9aae5e5900952ca
Sarojana Yalpalwar <yalpalwarsarojana@gmail.com>
2026-08-04 21:16:05 +0530

    Create Basic sum.c

---
+++ Basic sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+int a,b;
+printf("Enter a: ");
+scanf("%d", &a);
+
+printf("enter b: ");
+scanf("%d", &b);
+
+int sum = a + b;
+printf("sum is : %d",sum);
+return 0;                                                                                                                                                                                                                                                       
+}
