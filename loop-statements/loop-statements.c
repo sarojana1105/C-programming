f4bee966116d636ab22489a5db7d6dd0915a7950
Sarojana Yalpalwar <yalpalwarsarojana@gmail.com>
2026-08-10 22:03:37 +0530

    Add loop statements to print numbers from 1 to 50
    Implement for, while, and do-while loops to print numbers from 1 to 50.

---
+++ loop-statements/loop-statements.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+   //for loop
+     for(int i=1; i<=50 ;i++){
+            printf("%d\n", i);
+          }
+  
+    //while loop
+      int i = 1;
+      while(i<=50) {
+        printf("%d\n", i);
+        i++;
+        
+   }
+      
+     // do while loop
+      i = 1;
+      do {
+        printf("%d\n", i);
+        i++;
+      } while(i<=50);
+
+      return 0;
+   }
