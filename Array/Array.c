d9c7fda54351710a04352b08ac69e484d424381b
Sarojana Yalpalwar <yalpalwarsarojana@gmail.com>
2026-09-04 22:15:10 +0530

    Add program to input and display marks for subjects

---
+++ Array/Array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+  int main (){
+     
+     //int marks1 = 97;
+     //int marks2 = 98;
+     //int marks3 = 89;
+
+      int marks [3];
+      printf("enter phy : ");
+      scanf("%d", &marks[0]);
+
+      printf("enter math : ");
+      scanf("%d", &marks[1]);
+
+      printf("enter chem : ");
+      scanf("%d", &marks[2]);
+
+       printf("phy = %d, math = %d, chem = %d", marks[0], marks[1], marks[2]);
+       return 0 ;
+
+
+  }
