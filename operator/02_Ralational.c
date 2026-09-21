60b7a4065f47c57cbdb7bed0c62a3751631f8495
Sarojana Yalpalwar <yalpalwarsarojana@gmail.com>
2026-09-21 16:49:56 +0530

    Add relational operators demonstration in C

---
+++ operator/02_Ralational.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ 
+   int main(){
+     int a=10, b=20;
+   
+      printf("Ralational operator:\n");
+      printf("a < b :%d\n" , a<b);
+      printf("a > b :%d\n" , a>b);
+      printf("a <= b :%d\n" , a<=b);
+      printf("a >= b :%d\n" , a>=b);
+      printf("a == b : %d\n" , a==b);
+      printf("a != a :%d\n" , a!=b);
+      return 0;
+}
