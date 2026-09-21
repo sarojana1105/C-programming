940fa8445c4af84142ef5647d2705802890418b7
Sarojana Yalpalwar <yalpalwarsarojana@gmail.com>
2026-09-21 17:35:16 +0530

    Add files via upload

---
+++ 02_Operators/Logical.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+    int main(){
+
+    int a=10,b=5,c;
+    printf("LogicaL Operation:\n");
+    printf("(a>b) && (b>a) :  %d\n",(a>b) && (b>a) );
+    printf("(a<b) || (b<a) :  %d\n",(a<b) || (b<a) );
+    printf("! (a == b): %d\n", !(a == b) );
+    
+    return 0;
+  }
